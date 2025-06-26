722cbe6010e6439f5622e1af630535dbbe914409
Abhishek_Singhal <abhisheksinghal674@gmail.com>
2025-06-26 13:34:01 +0530

    routes corrected

--- backend/src/routes/song.route.js
+++ backend/src/routes/song.route.js
@@ -1,8 +1,9 @@
 import {Router } from 'express';
 import { getAllSongs, getFeaturedSongs, getMadeForYou, getTrendingSongs } from '../controller/song.controller.js';
+import { protectRoute, requireAdmin } from '../middleware/auth.middleware.js';
 const router =Router();
 
-router.get('/',getAllSongs);
+router.get('/',protectRoute,requireAdmin,getAllSongs);
 router.get("/featured",getFeaturedSongs);
 router.get("/trending",getTrendingSongs);
 router.get("/made-for-you",getMadeForYou)
